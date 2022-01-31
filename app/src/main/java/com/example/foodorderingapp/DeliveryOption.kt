5190f65a15e56be366b9083418ae3222dfc4ebea
Kinga Kotowicz <kingkot226@student.polsl.pl>
2022-02-01 00:04:39 +0100

    edit fragments and buttons connection

--- app/src/main/java/com/example/foodorderingapp/DeliveryOption.kt
+++ app/src/main/java/com/example/foodorderingapp/DeliveryOption.kt
@@ -5,55 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toolbar
+import androidx.navigation.findNavController
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DeliveryOption.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DeliveryOption : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_delivery_option, container, false)
+        val toolbar = view.findViewById<Toolbar>(R.id.toolbar_delivery_option)
+
+        toolbar.inflateMenu(R.menu.menu_back)
+        toolbar.setOnMenuItemClickListener {
+            if(it.itemId==R.id.back_button){
+                view.findNavController().navigate(R.id.action_deliveryOption_to_deliveryOrTakeoutChoice)
+            }
+            true
+        }
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_delivery_option, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DeliveryOption.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DeliveryOption().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view:View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.button_save_data).apply {
+            setOnClickListener {
+                view.findNavController().navigate(R.id.action_deliveryOption_to_cathegoriesList)
             }
+        }
     }
 }
--- app/src/main/java/com/example/foodorderingapp/fragments/CategoriesList.kt
+++ app/src/main/java/com/example/foodorderingapp/fragments/CategoriesList.kt
@@ -5,21 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toolbar
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foodorderingapp.R
 import com.example.foodorderingapp.databinding.FragmentCathegoriesListBinding
 import com.example.foodorderingapp.fragments.CategoriesAdapter
 
 
 class CategoriesList : Fragment() {
 
-    private var layoutManager: GridLayoutManager? = null
-    private var _binding: FragmentCathegoriesListBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var viewModel: CategoryViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    //private var layoutManager: GridLayoutManager? = null
+    //private var _binding: FragmentCathegoriesListBinding? = null
+    //private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,14 +31,47 @@ class CategoriesList : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        _binding =  FragmentCathegoriesListBinding.inflate(inflater, container, false)
-        val cathegoriesView = binding.cathegoriesRecyclerView
-        layoutManager = GridLayoutManager(activity, 2)
-        binding.cathegoriesRecyclerView.layoutManager = layoutManager
-        cathegoriesView.adapter = CategoriesAdapter(layoutManager)
+        //_binding =  FragmentCathegoriesListBinding.inflate(inflater, container, false)
+        //val cathegoriesView = binding.cathegoriesRecyclerView
+        //layoutManager = GridLayoutManager(activity, 2)
+        //binding.cathegoriesRecyclerView.layoutManager = layoutManager
+        //cathegoriesView.adapter = CategoriesAdapter(layoutManager)
 
+        //adding toolbar
+        val view = inflater.inflate(R.layout.fragment_cathegories_list, container, false)
+        val toolbar = view.findViewById<Toolbar>(R.id.toolbar_categories)
 
-        return binding.root
+        toolbar.inflateMenu(R.menu.menu_back)
+        toolbar.setOnMenuItemClickListener {
+            if(it.itemId==R.id.back_button){
+                //todo back to delivery or takeaway
+                view.findNavController().navigate(R.id.action_cathegoriesList_to_deliveryOption)
+            }
+            true
+        }
+
+        return inflater.inflate(R.layout.fragment_cathegories_list,container,false)
     }
+    /*override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CategoryViewModel::class.java)
+    }*/
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //categories-> List<Category> in viewmodel
+        viewModel = CategoryViewModel((requireNotNull(this.activity).application))
+
+        val category_adapter = CategoriesAdapter(view, viewModel, this.context, arguments)
+
+        viewModel.categories.observe(viewLifecycleOwner) {
+            category_adapter.notifyDataSetChanged()
+        }
+
+        val layoutManager = LinearLayoutManager(view.context)
+        view.findViewById<RecyclerView>(R.id.cathegoriesRecyclerView).let {
+            it.adapter = category_adapter
+            it.layoutManager = layoutManager
+        }
+    }
 }
